64a940b91e0382f644954946397f04a153ea3b41
Ian Marquez <regilmarquez123@gmail.com>
2025-05-31 19:58:46 +0800

    Initial Commit

---
+++ test/bilue.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Bilue from '../lib/bilue-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bilue-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Bilue.BilueStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
